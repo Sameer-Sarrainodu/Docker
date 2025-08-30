9166c379c34dbbf3497bbfebef298ab3ea1857f9
b27user12 <b27user12@rpsconsulting.in>
2025-08-30 11:08:15 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,56 @@
+resource "aws_instance" "roboshop-docker" {
+    ami = local.ami_id
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+    user_data = file("docker.sh")
+
+    root_block_device {
+      volume_size = 50
+      volume_type = "gp3"
+    }
+
+    tags = {
+        Name ="${var.project}-${var.environment}-docker"
+
+        docker="true"
+    }
+    
+    }
+
+
+
+  
+
+resource "aws_security_group" "allow_all_docker" {
+    name = "allow-all-docker" 
+    description = "sg for docker"
+
+    ingress {
+        
+        from_port=0
+        to_port=0
+        protocol=-1
+        cidr_blocks=["0.0.0.0/0"]
+        ipv6_cidr_blocks=["::/0"]
+        
+        
+    }
+    
+    egress{
+        from_port=0
+        to_port=0
+        protocol=-1
+        cidr_blocks=["0.0.0.0/0"]
+        ipv6_cidr_blocks=["::/0"]
+
+    }
+    
+
+    lifecycle {
+      create_before_destroy = true
+    }
+    tags = {
+      Name = "docker-sg"
+    }
+  
+}
