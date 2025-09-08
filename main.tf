9f7225cb410e8ca8fbbaf3fa9ff5ff136ec0ed89
b27user12 <b27user12@rpsconsulting.in>
2025-09-08 12:55:35 +0530

    hey

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "roboshop-docker" {
     ami = local.ami_id
-    instance_type = "t2.medium"
+    instance_type = "t3.medium"
     vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
-    user_data = file("docker.sh")
 
     root_block_device {
       volume_size = 50
       volume_type = "gp3"
     }
+    user_data = file("docker.sh")
 
     tags = {
         Name ="${var.project}-${var.environment}-docker"
