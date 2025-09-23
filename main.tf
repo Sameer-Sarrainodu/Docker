42a244d05454014dcc3d7fc7542a6080c28eada9
b27user12 <b27user12@rpsconsulting.in>
2025-09-23 10:09:17 +0530

    docker

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "roboshop-docker" {
     ami = local.ami_id
-    instance_type = "t3.medium"
+
+    # instance_type = "t3.medium"
+    instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
 
     root_block_device {
