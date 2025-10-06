214c94d0af3ba35281b9f8b602db957fe7727b99
Sameer-Sarrainodu <sameer339977.com>
2025-10-06 06:53:23 +0530

    kubernetes

--- docker.sh
+++ docker.sh
@@ -15,29 +15,29 @@ xfs_growfs /
 xfs_growfs /var
 
 
-# # for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
-# ARCH=amd64
-# PLATFORM=$(uname -s)_$ARCH
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
 
-# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 
-# # (Optional) Verify checksum
-# curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
 
-# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 
-# sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
 
-# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.5/2025-09-19/bin/linux/arm64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.5/2025-09-19/bin/linux/amd64/kubectl
 
-# chmod +x ./kubectl
+chmod +x ./kubectl
 
-# mv kubectl /usr/local/bin/kubectl
+mv kubectl /usr/local/bin/kubectl
 
-# eksctl version
-# kubectl version
+eksctl version
+kubectl version
 
-# sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
-# sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
-# sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
--- main.tf
+++ main.tf
@@ -4,6 +4,8 @@ resource "aws_instance" "roboshop-docker" {
     # instance_type = "t3.medium"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+    # iam_instance_profile ="TerraformAdmin" 
+    
 
     root_block_device {
       volume_size = 50
